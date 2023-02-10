eed43915c1eba88c77d8302fb9a54c80b8e1e0ca
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-10 02:40:09 -0600

    Add QuestDAO class to questie.persistence

---
+++ src/main/java/questie/persistence/QuestDAO.java
@@ -0,0 +1,36 @@
+package questie.persistence;
+
+import questie.entity.Quest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class QuestDAO {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    /**
+     * Gets all users
+     * @return all the users
+     */
+    public List<Quest> getAll() {
+
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Quest> query = builder.createQuery(Quest.class);
+        Root<Quest> root = query.from(Quest.class);
+        List<Quest> quests = session.createQuery(query).getResultList();
+        session.close();
+        return quests;
+    }
+}
