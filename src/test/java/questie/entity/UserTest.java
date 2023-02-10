99b44da572c5ea5c85f7a81f8f363ae5bda512e6
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-10 02:58:37 -0600

    Modified UserTest-this class isn't used, as there are no calculations in the User class

--- src/test/java/questie/entity/UserTest.java
+++ src/test/java/questie/entity/UserTest.java
@@ -8,23 +8,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserTest {
 
-    @Test
-    void getAge() {
-        // create the object that has the method that I want to test
-        User user = new User();
-
-        // set birthdate for the user
-        LocalDate birthDate = LocalDate.parse("1973-01-01");
-        user.setDateOfBirth(birthDate);
-
-        // create a variable for the expected value
-        int expectedAge = 50;
-
-        // call the method and get the actual value
-        int actualAge = user.getAge();
-
-        // compare the two values as pass or fail
-        assertEquals(expectedAge, actualAge);
-
-    }
 }
