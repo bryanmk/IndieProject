cc432429d480dfafdaa3c07309896669938daf03
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-11 12:51:04 -0600

    Added getByQuestName to QuestDAO

--- src/main/java/questie/persistence/QuestDAO.java
+++ src/main/java/questie/persistence/QuestDAO.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import questie.entity.User;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -29,6 +30,26 @@ public class QuestDAO {
         CriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<Quest> query = builder.createQuery(Quest.class);
         Root<Quest> root = query.from(Quest.class);
+        List<Quest> quests = session.createQuery(query).getResultList();
+        session.close();
+        return quests;
+    }
+
+    /**
+     * Gets quests by quest name
+     * @return quests by quest name
+     */
+    public List<Quest> getByQuestName(String questName) {
+
+        logger.debug("Searching for: {}", questName );
+
+        Session session = sessionFactory.openSession();
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Quest> query = builder.createQuery(Quest.class);
+        Root<Quest> root = query.from(Quest.class);
+        Expression<String> propertyPath = root.get("questName");
+        query.where(builder.like(propertyPath, "%" + questName + "%"));
         List<Quest> quests = session.createQuery(query).getResultList();
         session.close();
         return quests;
