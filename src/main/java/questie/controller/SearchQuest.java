cc74dc4bd5e9d30b27b8df2846c8b28a1388b165
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-11 12:54:36 -0600

    Add SearchQuest servlet to questie.controller package

---
+++ src/main/java/questie/controller/SearchQuest.java
@@ -0,0 +1,37 @@
+package questie.controller;
+
+import questie.persistence.QuestDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet to search for quests.
+ * @author mbryan
+ */
+
+@WebServlet(
+        urlPatterns = {"/searchUser"}
+)
+
+public class SearchUser extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        QuestDAO questDao = new QuestDAO();
+
+        if (req.getParameter("submit").equals("search")) {
+            req.setAttribute("users", questDao.getByQuestName(req.getParameter("searchTerm")));
+        } else {
+            req.setAttribute("users", questDao.getAll());
+        }
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/questSearchResults.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
