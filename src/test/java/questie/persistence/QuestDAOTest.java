4941900c79c42a0e6b0e589c79ef223f0953c65e
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-12 05:50:16 -0600

    QuestDAOTest functional except for updating a quest

--- src/test/java/questie/persistence/QuestDAOTest.java
+++ src/test/java/questie/persistence/QuestDAOTest.java
@@ -30,7 +30,7 @@ class QuestDAOTest {
     }
 
     /**
-     * Verifies getAllUsers success.
+     * Verifies getAllQuests success.
      */
     @Test
     void getAllQuestsSuccess() {
@@ -39,76 +39,76 @@ class QuestDAOTest {
     }
 
     /**
-     * Verifies getUsersByLastName success.
+     * Verifies getByQuestName success.
      */
-//    @Test
-//    void getUsersByLastNameSuccess() {
-//        List<Quest> quests = dao.getByLastName("c");
-//        assertEquals(3, quests.size());
-//    }
+    @Test
+    void getByQuestNameSuccess() {
+        List<Quest> quests = dao.getByQuestName("the");
+        assertEquals(3, quests.size());
+    }
 
     /**
-     * Verifies a user is returned correctly based on Id search.
+     * Verifies a quest is returned correctly based on Id search.
      */
-//    @Test
-//    void getByIdSuccess() {
-//        Quest retrievedQuest = dao.getById(3);
-//        assertNotNull(retrievedQuest);
-//        assertEquals("Barney", retrievedQuest.getQuestName());
-//    }
+    @Test
+    void getByQuestIdSuccess() {
+        Quest retrievedQuest = dao.getByQuestId(3);
+        assertNotNull(retrievedQuest);
+        assertEquals("Off To Area 52", retrievedQuest.getQuestName());
+    }
 
     /**
      * Verify successful insert of a user
      */
-//    @Test
-//    void insertSuccess() {
-//
-//        User newUser = new User("Fred", "Flintstone", "fflintstone");
-//        int id = dao.insert(newUser);
-//        assertNotEquals(0,id);
-//        User insertedUser = dao.getById(id);
-//        assertEquals("Fred", insertedUser.getFirstName());
-//    }
+    @Test
+    void insertSuccess() {
+
+        Quest newQuest = new Quest("Repeat After Me", 10, 1400, "Maldraxxus", "Signet of the Learned");
+        int id = dao.insert(newQuest);
+        assertNotEquals(0,id);
+        Quest insertedQuest = dao.getByQuestId(id);
+        assertEquals("Repeat After Me", insertedQuest.getQuestName());
+    }
 
     /**
-     * Verify successful delete of user
+     * Verify successful delete of quest
      */
-//    @Test
-//    void deleteSuccess() {
-//        dao.delete(dao.getById(3));
-//        assertNull(dao.getById(3));
-//    }
+    @Test
+    void deleteSuccess() {
+        dao.delete(dao.getByQuestId(3));
+        assertNull(dao.getByQuestId(3));
+    }
 
     /**
      * Verifies update success.
      */
 //    @Test
 //    void updateSuccess() {
-//        String newLastName = "Davis";
-//        User userToUpdate = dao.getById(3);
-//        userToUpdate.setLastName(newLastName);
-//        logger.info("new last name: " + newLastName);
-//        dao.saveOrUpdate(userToUpdate);
-//        User retrievedUser = dao.getById(3);
-//        assertEquals(newLastName, retrievedUser.getLastName());
+//        String newQuestName = "Cursed to Wither";
+//        Quest questToUpdate = dao.getByQuestId(1);
+//        questToUpdate.setQuestName(newQuestName);
+//        logger.info("new quest name: " + newQuestName);
+//        dao.saveOrUpdate(questToUpdate);
+//        Quest retrievedQuest = dao.getByQuestId(1);
+//        assertEquals(newQuestName, retrievedQuest.getQuestName());
 //    }
 
     /**
      * Verify successful get by property (equal match)
      */
-//    @Test
-//    void getByPropertyEqualSuccess() {
-//        List<User> users = dao.getByPropertyLike("lastName", "Curry");
-//        assertEquals(1, users.size());
-//        assertEquals(3, users.get(0).getId());
-//    }
+    @Test
+    void getByPropertyEqualSuccess() {
+        List<Quest> quests = dao.getByPropertyEqual("questName", "A Dying World");
+        assertEquals(1, quests.size());
+        assertEquals(1, quests.get(0).getQuestId());
+    }
 
     /**
      * Verify successful get by property (like match)
      */
-//    @Test
-//    void getByPropertyLikeSuccess() {
-//        List<User> users = dao.getByPropertyLike("lastName", "c");
-//        assertEquals(3, users.size());
-//    }
+    @Test
+    void getByPropertyLikeSuccess() {
+        List<Quest> quests = dao.getByPropertyLike("questName", "the");
+        assertEquals(3, quests.size());
+    }
 }
