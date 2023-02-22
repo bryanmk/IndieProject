491f2bc3e189e4c7e455ac6b391c64dbe44f4a2a
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-22 06:11:57 -0600

    Added GenericDAO class

---
+++ src/main/java/questie/persistence/GenericDAO.java
@@ -0,0 +1,72 @@
+package questie.persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class GenericDAO<T> {
+    private Class<T> type;
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    /**
+     * Instantiates a new generic dao
+     * @param type the entity type, for example: user, quest, favorite.
+     */
+    public GenericDAO(Class<T> type) {
+        this.type = type;
+    }
+
+    /**
+     * Returns an open session from the SessionFactory
+     * @return session
+     */
+    private Session getSession() {
+        return SessionFactoryProvider.getSessionFactory().openSession();
+    }
+
+    /**
+     * Gets an entity by id
+     * @param id entity id to search by
+     * @return an entity
+     * @param <T>
+     */
+    public <T>T getById(int id) {
+        Session session = getSession();
+        T entity = (T)session.get(type, id);
+        session.close();
+        return entity;
+    }
+
+    /**
+     * Deletes the entity
+     * @param entity entity to be deleted
+     */
+    public void delete(T entity) {
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(entity);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * Gets all entities
+     * @return all the entities
+     */
+    public List<T> getAll() {
+        Session session = getSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        List<T> list = session.createQuery(query).getResultList();
+        session.close();
+        return list;
+    }
+}
