9d6b7c674778e805dc99894caff7fc9d89411b17
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-24 06:21:37 -0600

    Another assertion added just for fun

--- src/test/java/questie/persistence/QuestApiDAOTest.java
+++ src/test/java/questie/persistence/QuestApiDAOTest.java
@@ -10,5 +10,6 @@ class QuestApiDAOTest {
     void getByQuestTitle() {
         QuestApiDAO dao = new QuestApiDAO();
         assertEquals(2, dao.getByQuestTitle("Sharptalon's Claw").getId());
+        assertEquals("Sharptalon's Claw", dao.getByQuestTitle("Sharptalon's Claw").getTitle());
     }
 }
