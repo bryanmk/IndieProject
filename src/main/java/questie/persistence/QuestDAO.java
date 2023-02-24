7f361fe93fc0dfc380cf16f74ce7fb04ce9a3b69
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-24 02:38:42 -0600

    Removed methods that have been moved to GenericDAO

--- src/main/java/questie/persistence/QuestDAO.java
+++ src/main/java/questie/persistence/QuestDAO.java
@@ -21,21 +21,6 @@ public class QuestDAO {
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
     /**
-     * Gets all quests
-     * @return all the quests
-     */
-    public List<Quest> getAll() {
-
-        Session session = sessionFactory.openSession();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<Quest> query = builder.createQuery(Quest.class);
-        Root<Quest> root = query.from(Quest.class);
-        List<Quest> quests = session.createQuery(query).getResultList();
-        session.close();
-        return quests;
-    }
-
-    /**
      * Gets quests by quest name
      * @return quests by quest name
      */
@@ -51,95 +36,6 @@ public class QuestDAO {
         Expression<String> propertyPath = root.get("questName");
         query.where(builder.like(propertyPath, "%" + questName + "%"));
         List<Quest> quests = session.createQuery(query).getResultList();
-        session.close();
-        return quests;
-    }
-
-    /**
-     * Gets a quest by Id
-     * @return quest
-     */
-    public Quest getByQuestId(int id) {
-        Session session = sessionFactory.openSession();
-        Quest quest = session.get(Quest.class, id);
-        session.close();
-        return quest;
-    }
-
-    /**
-     * insert quest
-     * @param quest  Quest to be inserted or updated
-     */
-    public int insert(Quest quest) {
-        int id = 0;
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        id = (int)session.save(quest);
-        transaction.commit();
-        session.close();
-        return id;
-    }
-
-    /**
-     * Delete a quest
-     * @param quest Quest to be deleted
-     */
-    public void delete(Quest quest) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(quest);
-        transaction.commit();
-        session.close();
-    }
-
-    /**
-     * update quest
-     * @param quest  Quest to be inserted or updated
-     */
-    public void saveOrUpdate(Quest quest) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.saveOrUpdate(quest);
-        transaction.commit();
-        session.close();
-    }
-
-    /**
-     * Get quest by property (exact match)
-     * sample usage: getByPropertyEqual("questName", "A Dying World")
-     */
-    public List<Quest> getByPropertyEqual(String propertyName, String value) {
-        Session session = sessionFactory.openSession();
-
-        logger.debug("Searching for quest with " + propertyName + " = " + value);
-
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<Quest> query = builder.createQuery( Quest.class );
-        Root<Quest> root = query.from( Quest.class );
-        query.select(root).where(builder.equal(root.get(propertyName), value));
-        List<Quest> quests = session.createQuery( query ).getResultList();
-
-        session.close();
-        return quests;
-    }
-
-    /**
-     * Get quest by property (like)
-     * sample usage: getByPropertyLike("questName", "the")
-     */
-    public List<Quest> getByPropertyLike(String propertyName, String value) {
-        Session session = sessionFactory.openSession();
-
-        logger.debug("Searching for user with {} = {}",  propertyName, value);
-
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<Quest> query = builder.createQuery( Quest.class );
-        Root<Quest> root = query.from( Quest.class );
-        Expression<String> propertyPath = root.get(propertyName);
-
-        query.where(builder.like(propertyPath, "%" + value + "%"));
-
-        List<Quest> quests = session.createQuery( query ).getResultList();
         session.close();
         return quests;
     }
--- src/main/java/questie/persistence/UserDAO.java
+++ src/main/java/questie/persistence/UserDAO.java
@@ -20,21 +20,6 @@ public class UserDAO {
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
     /**
-     * Gets all users
-     * @return all the users
-     */
-    public List<User> getAll() {
-
-        Session session = sessionFactory.openSession();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<User> query = builder.createQuery(User.class);
-        Root<User> root = query.from(User.class);
-        List<User> users = session.createQuery(query).getResultList();
-        session.close();
-        return users;
-    }
-
-    /**
      * Gets users by last name
      * @return users by last name
      */
@@ -50,96 +35,6 @@ public class UserDAO {
         Expression<String> propertyPath = root.get("lastName");
         query.where(builder.like(propertyPath, "%" + lastName + "%"));
         List<User> users = session.createQuery(query).getResultList();
-        session.close();
-        return users;
-    }
-
-    /**
-     * Gets a user by Id
-     * @return user
-     */
-    public User getById(int id) {
-        Session session = sessionFactory.openSession();
-        User user = session.get(User.class, id);
-        session.close();
-        return user;
-    }
-
-    /**
-     * update user
-     * @param user  User to be inserted or updated
-     */
-    public void saveOrUpdate(User user) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.saveOrUpdate(user);
-        transaction.commit();
-        session.close();
-    }
-
-    /**
-     * update user
-     * @param user  User to be inserted or updated
-     */
-    public int insert(User user) {
-        int id = 0;
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        id = (int)session.save(user);
-        transaction.commit();
-        session.close();
-        return id;
-    }
-
-    /**
-     * Delete a user
-     * @param user User to be deleted
-     */
-    public void delete(User user) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(user);
-        transaction.commit();
-        session.close();
-    }
-
-
-    /**
-     * Get user by property (exact match)
-     * sample usage: getByPropertyEqual("lastname", "Curry")
-     */
-    public List<User> getByPropertyEqual(String propertyName, String value) {
-        Session session = sessionFactory.openSession();
-
-        logger.debug("Searching for user with " + propertyName + " = " + value);
-
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<User> query = builder.createQuery( User.class );
-        Root<User> root = query.from( User.class );
-        query.select(root).where(builder.equal(root.get(propertyName), value));
-        List<User> users = session.createQuery( query ).getResultList();
-
-        session.close();
-        return users;
-    }
-
-    /**
-     * Get user by property (like)
-     * sample usage: getByPropertyLike("lastname", "C")
-     */
-    public List<User> getByPropertyLike(String propertyName, String value) {
-        Session session = sessionFactory.openSession();
-
-        logger.debug("Searching for user with {} = {}",  propertyName, value);
-
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<User> query = builder.createQuery( User.class );
-        Root<User> root = query.from( User.class );
-        Expression<String> propertyPath = root.get(propertyName);
-
-        query.where(builder.like(propertyPath, "%" + value + "%"));
-
-        List<User> users = session.createQuery( query ).getResultList();
         session.close();
         return users;
     }
