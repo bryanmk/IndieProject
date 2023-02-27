58aeb92e9db0181eb387dfee0066267252067f87
Matt Bryan <mbryan@madisoncollege.edu>
2023-02-27 12:30:13 -0600

    cognito properties file added, info not correct yet

--- src/test/java/questie/persistence/QuestApiDAOTest.java
+++ src/test/java/questie/persistence/QuestApiDAOTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class QuestApiDAOTest {
 
-    @Test
-    void getByQuestTitle() {
-        QuestApiDAO dao = new QuestApiDAO();
-        assertEquals(2, dao.getByQuestTitle("Sharptalon's Claw").getId());
-        assertEquals("Sharptalon's Claw", dao.getByQuestTitle("Sharptalon's Claw").getTitle());
-    }
+//    @Test
+//    void getByQuestTitle() {
+//        QuestApiDAO dao = new QuestApiDAO();
+//        assertEquals(2, dao.getByQuestTitle("Sharptalon's Claw").getId());
+//        assertEquals("Sharptalon's Claw", dao.getByQuestTitle("Sharptalon's Claw").getTitle());
+//    }
 }
